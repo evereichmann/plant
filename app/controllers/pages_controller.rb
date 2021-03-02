b418dfcdcf3927ad8196294042d497b443a50129
evereichmann <eve.reichmann@gmail.com>
2021-03-02 11:21:40 -0600

    added sold page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,4 +4,10 @@ class PagesController < ApplicationController
         @orders = Order.all 
         @cuttings = Cutting.all
     end
+
+    def sold
+        @plants = Plant.all
+        @orders = Order.all 
+        @cuttings = Cutting.all
+    end    
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
   
   root 'users#index'
   get 'pages/dashboard'
+  get 'pages/sold'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   # root 'welcome#index'
 end
